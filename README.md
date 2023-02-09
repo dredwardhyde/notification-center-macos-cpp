870c9088754683e020cd576249cda5505b913912
Nikita Schneider <komesergey@gmail.com>
2023-02-10 02:34:37 +0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Display Notifications in Notification Center From C++
+## Display Notifications in Notification Center From C++ Code
 
 <img src="https://raw.githubusercontent.com/dredwardhyde/notification-center-mac-cpp/main/demo.png" width="350"/>  
--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <objc/objc-runtime.h>
 
-int main(int argc, char** argv) {
-    Class bundle = objc_getClass("NSBundle");
-    class_replaceMethod(bundle,
+int main(int argc, char **argv) {
+    class_replaceMethod(objc_getClass("NSBundle"),
                         sel_registerName("bundleIdentifier"),
-                        method_getImplementation((Method)^{return CFSTR("com.apple.systempreferences");}),
+                        method_getImplementation((Method) ^{ return CFSTR("com.apple.systempreferences"); }),
                         nullptr);
-    id notificationCenter = ((id (*) (id, SEL))objc_msgSend)((id)objc_getClass("NSUserNotificationCenter"),
-                                  sel_registerName("defaultUserNotificationCenter"));
-    id notification = ((id (*) (id, SEL, SEL))objc_msgSend)((id)objc_getClass("NSUserNotification"),
-                            sel_registerName("alloc"),
-                            sel_registerName("init"));
-    ((id (*) (id, SEL, CFStringRef))objc_msgSend)(notification, sel_registerName("setTitle:"), CFSTR("Hi"));
-    ((id (*) (id, SEL, CFStringRef))objc_msgSend)(notification, sel_registerName("setSubtitle:"), CFSTR("Notification"));
-    ((id (*) (id, SEL, CFStringRef))objc_msgSend)(notification, sel_registerName("setInformativeText:"), CFSTR("From System Settings!"));
-    ((id (*) (id, SEL, id))objc_msgSend)(notificationCenter, sel_registerName("deliverNotification:"), notification);
+
+    id notificationCenter = ((id (*)(id, SEL)) objc_msgSend)((id) objc_getClass("NSUserNotificationCenter"),
+                                                             sel_registerName("defaultUserNotificationCenter"));
+    id notification = ((id (*)(id, SEL, SEL)) objc_msgSend)((id) objc_getClass("NSUserNotification"),
+                                                            sel_registerName("alloc"), sel_registerName("init"));
+
+    ((id (*)(id, SEL, CFStringRef)) objc_msgSend)(notification, sel_registerName("setTitle:"), CFSTR("Hi"));
+    ((id (*)(id, SEL, CFStringRef)) objc_msgSend)(notification, sel_registerName("setSubtitle:"), CFSTR("Notification"));
+    ((id (*)(id, SEL, CFStringRef)) objc_msgSend)(notification, sel_registerName("setInformativeText:"), CFSTR("From System Settings!"));
+
+    ((id (*)(id, SEL, id)) objc_msgSend)(notificationCenter, sel_registerName("deliverNotification:"), notification);
     sleep(1);
     return 0;
 }
